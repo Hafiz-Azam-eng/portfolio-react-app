6a901034406535bfb3c8ee130bd03ae44db9832b
Hafiz-Azam-eng <135889237+Hafiz-Azam-eng@users.noreply.github.com>
2024-03-10 13:01:05 -0700

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,7 +9,7 @@ export const About = () => {
       <h2 className={styles.title}>About</h2>
       <div className={styles.content}>
         <img
-          src={getImageUrl("about/download.png")}
+          src={getImageUrl("about/download.jpg")}
           alt="Me sitting with a laptop"
           className={styles.aboutImage}
         />
